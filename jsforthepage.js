5305e6ae3f7161e1c905d09f506d668baba0cdba
Vinterfred <hastur@startmail.com>
2023-06-12 22:52:23 +0200

    asdfgh

---
+++ jsforthepage.js
@@ -0,0 +1,26 @@
+function addOne(x) {
+  const blurp = document.getElementById(x);
+  blurp.innerHTML = Number(blurp.innerHTML) + 1;
+  return;
+}
+
+let init_coom = 10;
+let coom = 20;
+let sexyhaha = true;
+function sex() {
+    const sex = document.getElementById('sex')
+    sexyhaha = !sexyhaha;
+    if (sexyhaha) {
+        sex.innerHTML = "8={i}";
+        return
+    }
+    else if (coom <= 0) {
+        sex.innerHTML = "8===D~~~ {i}"
+        init_coom -= 1;
+        coom = init_coom
+    }
+    else {
+        coom -= 1
+        sex.innerHTML = "8===D {i}"
+    }
+}
